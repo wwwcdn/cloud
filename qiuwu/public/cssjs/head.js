6403c6a9f3ec371bcdaa9965dbd9c68a1d5f570b
wwwcdn <2015zhu.li@gmail.com>
2020-04-02 16:28:10 +0800

    cloud

--- qiuwu/public/cssjs/head.js
+++
@@ -1,8 +0,0 @@
-function showpop($url){
-	$j('#popbox').load($url,function(){
-		$j('#popbox').show().css('top', document.body.scrollTop+document.body.clientHeight-150).css('right', '0px');
-		setTimeout(function(){
-			$j("#popbox").hide();
-		}, 5000); 
-	});
-}
